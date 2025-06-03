17da770ea9e59a12d4c0b72b383f20c347d79faa
Ethan Hackney <ethanhackney@pop-os.lan>
2025-06-03 11:02:33 -0700

    added attributes

--- html/gen/lex.sh
+++ html/gen/lex.sh
@@ -5,6 +5,9 @@ cd "$(dirname "$0")"
 # read in tags
 mapfile -t tags <tagtab
 
+# read in attributes
+mapfile -t attrs <attrtab
+
 # generate start of ../html.h
 cat >../html.h <<EOF
 /* AUTO-GENERATED */
@@ -16,32 +19,49 @@ cat >../html.h <<EOF
 /* html token types */
 enum {
         HTML_TT_TAG_START = 1, /* start of tags */
-        HTML_TT_DOCTYPE, /* <!DOCTYPE */
 EOF
 
 # generate open tags
 echo -e "\tHTML_TT_TAG_OPEN_START, /* start of open tags */" >>../html.h
+echo -e "\tHTML_TT_TAG_DOCTYPE, /* <!DOCTYPE> */" >>../html.h
 for tag in "${tags[@]}"; do
-  echo -e "\tHTML_TT_${tag^^}_OPEN, /* <${tag}> */" >>../html.h
+  echo -e "\tHTML_TT_TAG_${tag^^}_OPEN, /* <${tag}> */" >>../html.h
 done
-echo -e "\tHTML_TT_COMMENT_OPEN, /* <!-- */" >>../html.h
+echo -e "\tHTML_TT_TAG_COMMENT_OPEN, /* <!-- */" >>../html.h
 echo -e "\tHTML_TT_TAG_OPEN_END, /* end of open tags */" >>../html.h
+echo -e "\tHTML_TT_TAG_END, /* end of tags */" >>../html.h
 
 # generate close tags
 echo -e "\tHTML_TT_TAG_CLOSE_START, /* start of close tags */" >>../html.h
 for tag in "${tags[@]}"; do
-  echo -e "\tHTML_TT_${tag^^}_CLOSE, /* </${tag}> */" >>../html.h
+  echo -e "\tHTML_TT_TAG_${tag^^}_CLOSE, /* </${tag}> */" >>../html.h
 done
-echo -e "\tHTML_TT_COMMENT_CLOSE, /* --> */" >>../html.h
+echo -e "\tHTML_TT_TAG_COMMENT_CLOSE, /* --> */" >>../html.h
 echo -e "\tHTML_TT_TAG_CLOSE_END, /* end of close tags */" >>../html.h
+
+# generate attributes
+echo -e "\tHTML_TT_ATTR_START, /* start of attributes */" >>../html.h
+for attr in "${attrs[@]}"; do
+  name=$attr
+  if [ $attr = 'accept-charset' ]; then
+    name='ACCEPT_CHARSET'
+  fi
+  if [ $attr = 'http-equiv' ]; then
+    name='HTTP_EQUIV'
+  fi
+  if [ $attr = 'start' ]; then
+    name='START_ATTR'
+  fi
+  echo -e "\tHTML_TT_ATTR_${name^^}, /* ${attr} */" >>../html.h
+done
+echo -e "\tHTML_TT_ATTR_END, /* end of attributes */" >>../html.h
 
 # generate end of ../html.h
 cat >>../html.h <<EOF
-        HTML_TT_TAG_END, /* end of tags */
-        HTML_TT_OPEN_DONE, /* done with opening tag? */
+        HTML_TT_TAG_OPEN_DONE, /* done with opening tag? */
         HTML_TT_TEXT, /* regular text */
         HTML_TT_ERR_START, /* start of errors */
-        HTML_TT_TAG_UNTERM, /* unmterminated tag */
+        HTML_TT_ERR_TAG_UNTERM, /* unmterminated tag */
         HTML_TT_ERR_END, /* end of errors */
         HTML_TT_COUNT, /* type count */
 };
@@ -93,7 +113,7 @@ static bool in_open_tag = false;
 %%
 <<EOF>> {
         if (in_open_tag)
-                return HTML_TT_TAG_UNTERM;
+                return HTML_TT_ERR_TAG_UNTERM;
 
         return 0;
 }
@@ -101,26 +121,48 @@ EOF
 
 # generate flex for close tag
 for tag in "${tags[@]}"; do
-  echo "\"</${tag}>\" { return  HTML_TT_${tag^^}_CLOSE; }" >>../html.l
+  echo "\"</${tag}>\" { return  HTML_TT_TAG_${tag^^}_CLOSE; }" >>../html.l
 done
-echo '"<!--" { return HTML_TT_COMMENT_OPEN; }' >>../html.l
-echo '"<!DOCTYPE" { return HTML_TT_DOCTYPE; }' >>../html.l
+echo '"<!--" { return HTML_TT_TAG_COMMENT_OPEN; }' >>../html.l
 
 # generate flex for open tag
+cat >>../html.l <<EOF
+"<!DOCTYPE" {
+        in_open_tag = true;
+        return HTML_TT_TAG_DOCTYPE;
+}
+EOF
 for tag in "${tags[@]}"; do
   cat >>../html.l <<EOF
 "<${tag}" {
         in_open_tag = true;
-        return HTML_TT_${tag^^}_OPEN;
+        return HTML_TT_TAG_${tag^^}_OPEN;
 }
 EOF
 done
 
+# generate flex for attributes
+for attr in "${attrs[@]}"; do
+  name=$attr
+  if [ $attr = 'accept-charset' ]; then
+    name='ACCEPT_CHARSET'
+  fi
+  if [ $attr = 'http-equiv' ]; then
+    name='HTTP_EQUIV'
+  fi
+  if [ $attr = 'start' ]; then
+    name='START_ATTR'
+  fi
+  cat >>../html.l <<EOF
+"${attr}" { return HTML_TT_ATTR_${name^^}; }
+EOF
+done
+
 cat >>../html.l <<EOF
-"-->" { return HTML_TT_COMMENT_CLOSE; }
+"-->" { return HTML_TT_TAG_COMMENT_CLOSE; }
 ">" {
         in_open_tag = false;
-        return HTML_TT_OPEN_DONE;
+        return HTML_TT_TAG_OPEN_DONE;
 }
 .|\n {
         if (!in_open_tag)
